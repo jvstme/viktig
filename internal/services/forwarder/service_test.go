c5b7782a134f5a6e817fd1767f2723631d380a6f
Egor Nazarov <nazarov.egor2013@gmail.com>
2024-04-27 01:24:56 +0300

    fix and refactor tests

--- internal/services/forwarder/service_test.go
+++ internal/services/forwarder/service_test.go
@@ -2,19 +2,20 @@ package forwarder
 
 import (
 	"testing"
-	"viktig/internal/core"
+	"viktig/internal/entities"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRender(t *testing.T) {
-	actual := render(core.Message{Text: "Hello", VkSenderId: 1234})
-	expected := "👤 <a href=\"https://vk.com/id1234\">1234</a>\n💬 Hello"
-	assert.Equal(t, expected, actual)
-}
-
-func TestRenderEscapesHTML(t *testing.T) {
-	actual := render(core.Message{Text: "<a href=\"https://x.com\">&</a>", VkSenderId: 1})
-	expected := "👤 <a href=\"https://vk.com/id1\">1</a>\n💬 &lt;a href=&#34;https://x.com&#34;&gt;&amp;&lt;/a&gt;"
-	assert.Equal(t, expected, actual)
+	t.Run("basic render", func(t *testing.T) {
+		actual := render(entities.Message{Text: "Hello", VkSenderId: 1234})
+		expected := "👤 <a href=\"https://vk.com/id1234\">1234</a>\n💬 Hello"
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("escape HTML", func(t *testing.T) {
+		actual := render(entities.Message{Text: "<a href=\"https://x.com\">&</a>", VkSenderId: 1})
+		expected := "👤 <a href=\"https://vk.com/id1\">1</a>\n💬 &lt;a href=&#34;https://x.com&#34;&gt;&amp;&lt;/a&gt;"
+		assert.Equal(t, expected, actual)
+	})
 }
