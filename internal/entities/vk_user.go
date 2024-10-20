26d1f6f995bac4e5e3e4b9a0b4c92941b124a884
jvstme <git@jvst.me>
2024-10-20 12:15:06 +0200

    Add name rendering test

--- internal/entities/vk_user.go
+++ internal/entities/vk_user.go
@@ -1,9 +1,6 @@
 package entities
 
 type VkUser struct {
-	VkUserId         int    `json:"id"`
-	FirstName        string `json:"first_name"`
-	LastName         string `json:"last_name"`
-	CanAccessPrivate bool   `json:"can_access_closed"`
-	IsPrivateProfile bool   `json:"is_closed"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
